f6fce82d860bef619815850d4cf267ce3cd2d04f
Dmitriy <archangel2903@gmail.com>
2019-04-26 13:10:52 +0300

    end

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import '../scss/main.scss';
 import $ from 'jquery';
 import 'bootstrap';
 import 'popper.js';
+import 'slick-carousel'
 
 $(window).on('load', function () {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
@@ -13,10 +14,34 @@ $(window).on('load', function () {
     $('body').removeClass('loaded');
 });
 
-$(function () {
-    $('.section-bg').each(function () {
-        $(this).data('img-bg');
+$(window).on('load resize', function () {
+    $('.scroll-down').on('click', function (e) {
+        $('html, body').stop().animate({
+            scrollTop: window.innerHeight
+        }, 1000);
+        e.preventDefault();
     });
 
-    $('.main-section').height(window.innerHeight);
+    if (window.innerHeight >= 550) {
+        $('.main-section').height(window.innerHeight);
+    }
+});
+
+$(function () {
+    $('.slick-slider').each(function () {
+        if($(this).hasClass('reviews')){
+
+        }
+
+        $(this).slick({
+            arrows: true,
+            prevArrow: '<button class="slick-prev slick-arrow"><svg><use xlink:href="img/spritemap.svg#sprite-chevron-left"></use></svg></button>',
+            nextArrow: '<button class="slick-next slick-arrow"><svg><use xlink:href="img/spritemap.svg#sprite-chevron-right"></use></svg></button>',
+            dots: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            adaptiveHeight: true
+        });
+    });
 });
